f42fdad715e8d20a4ea3348eaded002ee0777c80
Marian Bonhomme <marian.bonhomme@gmail.com>
2024-01-21 10:31:08 +0100

    page localisation

--- src/pages/public/LocalisationPage.jsx
+++ src/pages/public/LocalisationPage.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from "react";
 
 function LocalisationPage() {
   return (
-	<div>Localisation</div>
-  )
+
+    <div className="min-h-[90dvh] flex flex-col justify-between p-20">
+      <h2 className="font-lora text-7xl font-bold mb-10">LOCALISATION</h2>
+      <div className="flex justify-center items-center">
+        <div className="w-1/3 select-none">
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d51870596.56745394!2d-40.28698029444197!3d37.485979003860415!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14a838db23a7969d%3A0xf8152979342cc024!2sSPORTFOOD!5e0!3m2!1sfr!2sfr!4v1705828087190!5m2!1sfr!2sfr"
+            className="rounded-2xl shadow-custom "
+            width="400"
+            height="400"
+            loading="lazy"
+          ></iframe>
+        </div>
+        <div className="w-1/3 relative">
+          <ul className="flex flex-col gap-5 text-xl">
+            <li><span className="font-bold mr-3">Téléphone:</span>06 00 00 00 00</li>
+            <li><span className="font-bold mr-3">Email:</span>sportfood@gmail.com</li>
+            <li><span className="font-bold mr-3">Adresse:</span>Leof. Kon/nou Karamanli 125, Thessaloniki 542 49, Grèce</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default LocalisationPage
+export default LocalisationPage;
--- src/pages/public/PresentationPage.jsx
+++ src/pages/public/PresentationPage.jsx
@@ -11,23 +11,23 @@ function PresentationPage() {
           <p className='text-xl text-justify mb-8'>Découvrez le plaisir d'une cuisine saine et sportive chez nous. Nos plats délicieux, riches en saveur et en nutriments, sont conçus pour les amateurs de bien-être et de sport. Rejoignez-nous pour une expérience culinaire qui nourrit votre corps et votre esprit. Bienvenue chez Sport Food!</p>
           <div className='flex gap-5'>
             <NavLink to={"/carte"} >
-              <button className='bg-green text-white px-6 py-3 rounded-3xl'>Découvrir la carte</button>
+              <button className='bg-green text-white px-6 py-3 rounded-3xl select-none'>Découvrir la carte</button>
             </NavLink>
             <NavLink to={"/localisation"} >
-              <button className='bg-orange text-white px-6 py-3 rounded-3xl'>Nous rendre visite</button>
+              <button className='bg-orange text-white px-6 py-3 rounded-3xl select-none'>Nous rendre visite</button>
             </NavLink>
           </div>
         </div>
-        <img src="src/assets/img/headline-ver.jpg" className='absolute bottom-0 right-0 h-full' />
+        <img src="src/assets/img/headline-ver.jpg" className='absolute bottom-0 right-0 h-full select-none' />
       </div>
       <div className="w-full flex justify-center items-center mt-20">
-        <img src="src/assets/img/dish.png" className='w-3/12'/>
+        <img src="src/assets/img/dish.png" className='w-3/12 select-none'/>
         <div className='w-1/2 h-full relative flex flex-col justify-center pl-32 z-10'>
           <h2 className='text-6xl font-lora font-bold mb-5 text-nowrap'>Healthy Food</h2>
           <p className='text-justify'>À destination des amateurs de sport cherchant à maintenir leur bien-être, notre restaurant se distingue par une offre culinaire dédiée. Chaque plat, équilibré et savoureux, est minutieusement conçu pour répondre aux exigences d'une clientèle active en quête d'une alimentation saine et délicieuse.</p>
           <div className='flex gap-5 mt-6'>
             <NavLink to={"/carte"} >
-              <button className='bg-orange text-white px-6 py-3 rounded-3xl'>Je commande!</button>
+              <button className='bg-green text-white px-6 py-3 rounded-3xl select-none'>Je commande</button>
             </NavLink>
           </div>
         </div>
@@ -35,15 +35,15 @@ function PresentationPage() {
       <div className="w-full flex justify-center items-center mb-20">
         <div className='w-7/12 h-full relative flex flex-col justify-center pl-32 z-10'>
           <h2 className='text-6xl font-lora font-bold mb-5 text-nowrap'>Tracking des Nutriments</h2>
-          <p className='text-justify mb-3'>Nous sommes conscients de l'importance d'une alimentation rigoureuse. C'est pourquoi nous détaillons la composition en macronutriments de chacun de nos plats. Protéines, glucides, lipides... aucune information ne vous échappera.</p>
+          <p className='text-justify mb-3'>Nous sommes conscients de l'importance d'une alimentation rigoureuse. C'est pourquoi nous détaillons la composition en macronutriments de chacun de nos plats. <span className='text-orange font-medium'>Protéines, glucides, lipides...</span> aucune information ne vous échappera.</p>
           <p className='text-justify'>Avec Sport Food, adopter une alimentation saine au quotidien devient une démarche aisée ! Vous pourrez ainsi poursuivre votre régime en toute simplicité grâce à notre engagement à vous offrir une transparence totale sur la composition de nos repas.</p>
           <div className='flex gap-5 mt-6'>
             <NavLink to={"/carte"} >
-              <button className='bg-green text-white px-6 py-3 rounded-3xl'>C'est pour moi</button>
+              <button className='bg-orange text-white px-6 py-3 rounded-3xl select-none'>C'est pour moi!</button>
             </NavLink>
           </div>
         </div>
-        <img src="src/assets/img/pyramid.jpg" className='w-1/3 max-w-xl'/>
+        <img src="src/assets/img/pyramid.jpg" className='w-1/3 max-w-xl select-none'/>
       </div>
     </div>
   )
